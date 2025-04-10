1440a2eb9951c4867a673cd6c95dc2818e510879
Fali.lp <falilealp@gmail.com>
2025-04-10 18:09:50 +0200

    📦 Build:

---
+++ crudService/crudService/src/main/java/com/crudService/crudService/CrudServiceApplication.java
@@ -0,0 +1,11 @@
+package com.crudService.crudService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CrudServiceApplication.class, args);
+	}
+}
