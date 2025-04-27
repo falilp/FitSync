c7b3917f111b2c850cd8421f40a2bc2e197f7fd4
Fali.lp <falilealp@gmail.com>
2025-04-27 18:07:45 +0200

    ⚡ Performance: dto`s

--- authService/authService/src/main/java/com/authService/authService/dto/AuthRequest.java
+++ authService/authService/src/main/java/com/authService/authService/dto/AuthRequest.java
@@ -3,4 +3,17 @@ package com.authService.authService.dto;
 public class AuthRequest{
     private String username;
     private String password;
+
+    public AuthRequest(){}
+
+    public AuthRequest(String username, String pass){
+        this.username = username;
+        this.password = pass;
+    }
+
+    public String getUsername(){ return username; }
+    public String getPassword(){ return password; }
+
+    public void setUsername(String username){ this.username = username; }
+    public void setPassword(String pass){ this.password = pass; }
 }
--- authService/authService/src/main/java/com/authService/authService/dto/RegisterRequest.java
+++ authService/authService/src/main/java/com/authService/authService/dto/RegisterRequest.java
@@ -1,6 +1,19 @@
 package com.authService.authService.dto;
 
 public class RegisterRequest{
-    private String name;
-    private String password;    
+    private String username;
+    private String password;
+
+    public RegisterRequest(){}
+
+    public RegisterRequest(String username, String pass){
+        this.username = username;
+        this.password = pass;
+    }
+
+    public String getUsername(){ return username; }
+    public String getPassword(){ return password; }
+
+    public void setUsername(String username){ this.username = username; }
+    public void setPassword(String pass){ this.password = pass; }  
 }
