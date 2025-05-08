8f064604bfde7166d4eb991a64b34b83355bfdb1
Fali.lp <falilealp@gmail.com>
2025-05-08 17:35:11 +0200

    🐛 Fix: Adaptation of the AuthService

--- authService/authService/src/main/java/com/authService/authService/dto/ResgisterResponse.java
+++ authService/authService/src/main/java/com/authService/authService/dto/ResgisterResponse.java
@@ -2,12 +2,18 @@ package com.authService.authService.dto;
 
 public class ResgisterResponse{
     private String msg;
+    private String token;
 
     public ResgisterResponse(){}
 
-    public ResgisterResponse(String msg){ this.msg = msg; }
+    public ResgisterResponse(String msg, String token){ 
+        this.msg = msg; 
+        this.token = token;
+    }
 
     public String getMsg(){ return msg; }
+    public String getToken(){ return token; }
 
     public void setMsg(String msg){ this.msg = msg; }
+    public void setToken(String token){ this.token = token; }
 }
--- authService/authService/src/main/java/com/authService/authService/services/AuthService.java
+++ authService/authService/src/main/java/com/authService/authService/services/AuthService.java
@@ -11,33 +11,49 @@ import com.authService.authService.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 //#endregion
 
 @Service
 public class AuthService{
-    private final JwtService jwt;
-    private final UserRepository userRepo;
-    private final PasswordEncoder passEncoder;
-    private final AuthenticationManager authManager;
+    @Autowired
+    private JwtService jwt;
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private PasswordEncoder passEncoder;
+    @Autowired
+    private AuthenticationManager authManager;
 
-    public AuthService(JwtService jwt, UserRepository repo, PasswordEncoder Encoder, AuthenticationManager manager){
+    // Eviatmos el consttrustor gracias a la inyección de dependencias del @AutoWired
+    /*public AuthService(JwtService jwt, UserRepository repo, PasswordEncoder Encoder, AuthenticationManager manager){
         this.jwt = jwt;
         this.userRepo = repo;
         this.passEncoder = Encoder;
         this.authManager = manager;
-    }
+    }*/
 
     public ResgisterResponse register(RegisterRequest req){
         User user = new User();
         
         user.setUserName(req.getUsername());
         user.setPassword(passEncoder.encode(req.getPassword()));
-        
+        user.setName(req.getName());
+        user.setSurName(req.getSurName());
+        user.setEmail(req.getEmail());
+        user.setBirthDate(req.getBirthDate());
+        user.setRole(req.getRol()); 
+        user.setGender(req.getGender());
+        user.setHeight(req.getHeight());
+        user.setWeight(req.getWeight());
+    
         userRepo.save(user);
 
-        return new ResgisterResponse("Exito al crear el usuario");
+        String token = jwt.buildToken(null, user.getUsername());
+
+        return new ResgisterResponse("Exito al crear el usuario", token);
     }
 
     public AuthResponse login(AuthRequest req){
